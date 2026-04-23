6ccfcfcf3c46c7130fba6236d4b703d11db128a7
Thamindu <tharu20060116@gmail.com>
2026-04-23 13:53:09 +0530

    new deployment 2

--- test_sec.py
+++ test_sec.py
@@ -50,5 +50,4 @@ def ping_server():
     return "Ping function has been secured/disabled."
 
 if __name__ == '__main__':
-    # host='0.0.0.0' අනිවාර්යයි
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False) # nosemgrep
